392adf46ace03b01452ce1a7c277542816c0ce0f
Joshua C. Magoliman <magolimanjoshua22@gmail.com>
2020-06-10 10:27:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ROCK PAPER SCISSORS GAME
+
+* Purpose: My Project
+* Front End: HTML5, CSS3, Vanilla JavaScript
+* Responsive Web Design: No
+* Type of Website: Dynamic
+
+<h2> User Interface Screenshot </h2> 
+  <img src="SCREENSHOT/PIC1.png">
+    
---
+++ vanilla js/main.js
@@ -0,0 +1,57 @@
+﻿// Created by: Joshua C. Magoliman
+let humanScore = 0;
+let computerScore = 0;
+document.getElementById('rock').onclick = playRock;
+document.getElementById('paper').onclick = playPaper;
+document.getElementById('scissors').onclick = playScissors;
+function playRock() {
+  play("rock");
+}
+function playPaper() {
+  play("paper");
+}
+function playScissors() {
+  play("scissors");
+}
+function play(humanPlay) {
+  computerPlay = getComputerPlay();
+  document.getElementById('status').innerHTML = "<p>(You = <b>" + humanPlay.toUpperCase() + "</b>) VS (AI = <b>" + computerPlay.toUpperCase() + "</b>)</p>";
+  if (humanPlay == 'rock') {
+    if (computerPlay == 'rock') {
+      document.getElementById('status').innerHTML += "<p>Result: Draw</p>";
+    } else if (computerPlay == 'paper') {
+      document.getElementById('status').innerHTML += "<p>Result: You lose</p>";
+      computerScore++;
+    } else if (computerPlay == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>Result: You win</p>";
+      humanScore++;
+    }
+  } else if (humanPlay == 'paper') {
+    if (computerPlay == 'rock') {
+      document.getElementById('status').innerHTML += "<p>Result: You win</p>";
+      humanScore++;
+    } else if (computerPlay == 'paper') {
+      document.getElementById('status').innerHTML += "<p>Result: Draw</p>";
+    } else if (computerPlay == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>Result: You lose</p>";
+      computerScore++;
+    }
+  } else if (humanPlay == 'scissors') {
+    if (computerPlay == 'rock') {
+      document.getElementById('status').innerHTML += "<p>Result: You lose</p>";
+      computerScore++;
+    } else if (computerPlay == 'paper') {
+      document.getElementById('status').innerHTML += "<p>Result: You win</p>";
+      humanScore++;
+    } else if (computerPlay == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>Result: Draw</p>";
+    }
+  }
+  document.getElementById('humanScore').innerHTML = humanScore;
+  document.getElementById('computerScore').innerHTML = computerScore;
+}
+function getComputerPlay() {
+  var plays = ['rock', 'paper', 'scissors'];
+  var play = plays[Math.floor(Math.random() * plays.length)];
+  return play;
+}
